94b4b020a899b540a118efe8aab102e0a43004bb
mikesmithlab <mikesmithlab@gmail.com>
2021-02-04 17:04:50 +0000

    SelectAreaWidget returning points

---
+++ testscript.py
@@ -0,0 +1,7 @@
+from particletracker import track_gui
+
+
+filename='/home/mike/Documents/Programming/python/ParticleTracker/particletracker/testdata/contours.mp4'
+settings_filename='/home/mike/Documents/Programming/python/ParticleTracker/particletracker/testdata/contours.param'
+
+track_gui(movie=filename,settings=settings_filename)
