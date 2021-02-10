39bbd0d1531f9c33122aca6deb5a806dfe45996a
mikesmithlab <mikesmithlab@gmail.com>
2021-02-10 07:37:04 +0000

    test

--- testscript.py
+++ testscript.py
@@ -12,3 +12,4 @@ from particletracker.general.param_file_creator import create_param_file
 create_param_file(settings_filename)
 
 track_gui(movie=filename,settings=settings_filename)
+print('test')
