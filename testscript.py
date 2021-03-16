9a9b44855ab6852de36b9633dd9227c8580057b0
mikesmithlab <mikesmithlab@gmail.com>
2021-03-16 17:15:55 +0000

    vectors in annotation methods now working

--- testscript.py
+++ testscript.py
@@ -5,7 +5,7 @@ import particletracker
 movie_filename='C:\\Users\\ppzmis\\OneDrive - The University of Nottingham\\Documents\\Programming\\particletracker\\testdata\\contours.mp4'
 settings_filename='C:\\Users\\ppzmis\\OneDrive - The University of Nottingham\\Documents\\Programming\\particletracker\\particletracker\\testdata\\contours.param'
 #settings_filename='C:\\Users\\ppzmis\\OneDrive - The University of Nottingham\\Documents\\Programming\\particletracker\\particletracker\\particletracker\\project\\default.param'
-#create_param_file(settings_filename)
+create_param_file(settings_filename)
 
 track_gui(movie_filename=movie_filename,settings_filename=settings_filename)
 
