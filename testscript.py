77e76ac3bebfc0056dbeb2496155e166344ab124
mikesmithlab <mikesmithlab@gmail.com>
2021-03-30 14:59:42 +0100

    added final testdata as zip

--- testscript.py
+++ testscript.py
@@ -2,10 +2,10 @@ from particletracker import track_gui
 from particletracker.general.param_file_creator import create_param_file
 import particletracker
 
-movie_filename='C:\\Users\\ppzmis\\OneDrive - The University of Nottingham\\Documents\\Programming\\particletracker\\testdata\\colloids.mp4'
-settings_filename='C:\\Users\\ppzmis\\OneDrive - The University of Nottingham\\Documents\\Programming\\particletracker\\testdata\\colloids.param'
+#movie_filename='C:\\Users\\ppzmis\\OneDrive - The University of Nottingham\\Documents\\Programming\\particletracker\\testdata\\colloids.mp4'
+#settings_filename='C:\\Users\\ppzmis\\OneDrive - The University of Nottingham\\Documents\\Programming\\particletracker\\testdata\\colloids.param'
 
 #create_param_file(settings_filename)
 
-#track_gui(movie_filename=movie_filename,settings_filename=settings_filename)
+track_gui()
 
