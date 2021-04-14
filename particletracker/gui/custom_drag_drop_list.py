5a168c18df238f29835152c24e37873b298ae0be
Mike Smith <mikesmithlab@gmail.com>
2021-04-14 17:24:54 +0100

    Update custom_drag_drop_list.py

--- particletracker/gui/custom_drag_drop_list.py
+++ particletracker/gui/custom_drag_drop_list.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from PyQt5.QtGui import QListWidget
+from PyQt5.QtGui import QListWidget, pyqtSignal
 
 
 class MyListWidget(QListWidget):   
