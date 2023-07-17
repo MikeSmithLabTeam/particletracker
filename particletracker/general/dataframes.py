ad378d9530ec2a15fe6dd5b0804321e41786f1e9
PPYOL1 <ppyol1@nottingham.ac.uk>
2023-07-17 15:11:16 +0100

    debugging postprocessing features

--- particletracker/general/dataframes.py
+++ particletracker/general/dataframes.py
@@ -103,7 +103,7 @@ class DataStore:
         data_dict['frame'] = frame
         new_df = pd.DataFrame(data_dict).set_index('frame')
         self.df = self.df.append(new_df)
-        #self.df = pd.concat([self.df, new_df.set_index('frame')])
+        
 
     def append_store(self, store):
         """
--- particletracker/postprocess/__init__.py
+++ particletracker/postprocess/__init__.py
@@ -13,17 +13,14 @@ class PostProcessor:
 
     def process(self, f_index=None, use_part=False):
         if f_index is None:
-            input_filename = self.data_filename
-            output_filename = self.data_filename
+            data_filename = self.data_filename
         else:
             if use_part:
-                input_filename = self.data_filename
-                output_filename = self.data_filename
+                data_filename = self.data_filename
             else:
-                input_filename = self.data_filename[:-5] + '_temp.hdf5'
-                output_filename = self.data_filename[:-5] + '_temp.hdf5'
-
-        with DataStore(input_filename, load=True) as data:
+                data_filename = self.data_filename[:-5] + '_temp.hdf5'
+                
+        with DataStore(data_filename, load=True) as data:
             if f_index is None:
                 frame_range = self.parameters['experiment']['frame_range']
                 start=frame_range[0]
@@ -41,6 +38,6 @@ class PostProcessor:
                     method_name, call_num = get_method_name(method)
                     data.df = getattr(pm, method_name)(data.df, f_index=f,parameters=self.parameters, call_num=call_num)
 
-            data.save(filename=output_filename)
+            data.save(filename=data_filename)
 
 
