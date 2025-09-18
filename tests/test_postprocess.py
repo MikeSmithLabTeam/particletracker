3a1790bda592bb722304850f3a0b34a398d17dfb
mikesmithlab <mikesmithlab@gmail.com>
2025-09-18 17:28:14 +0100

    merge

--- tests/test_postprocess.py
+++ tests/test_postprocess.py
@@ -3,7 +3,6 @@ import pandas as pd
 from particletracker.postprocess.postprocessing_methods import hexatic_order
 import scipy.spatial as sp
 
-<<<<<<< HEAD
 import pandas as pd
 import os
 import shutil
@@ -54,20 +53,3 @@ def test_postprocess():
     os.remove(output_df)
     if os.path.exists(temp_dir):
         shutil.rmtree(temp_dir)
-||||||| 64f60168
-=======
-def test_hexatic_order():
-    """Testing that the hexatic_order() function in particle 
-     tracker postprocessing methods works. Has the param parse
-      decorator attached. """
-    
-    param = {'postprocess':{'hexatic_order':{
-            'cutoff':[5]}}}
-    
-    data = pd.read_hdf("testdata/hexatic_test_data.hdf5")
-    data = hexatic_order(data, f_index=0, parameters=param, call_num=None, section='postprocess')
-    hexatic_col = data["hexatic_order"]
-    value = hexatic_col.iloc[[3][0]]    #pick particle away from edge
-    assert int(np.real(value)) == 1
-    assert int(np.imag(value)) == 0
->>>>>>> 3b7588ff46e3cbbcbcca6525623880719a0f83d2
