81d85bc6a064adcde40db6c2ce99c47b5c589d16
PPYOL1 <ppyol1@nottingham.ac.uk>
2025-10-14 10:43:57 +0100

    legacy version

--- launch.py
+++ launch.py
@@ -1,8 +1,12 @@
 #import particletracker as pt
 from particletracker import track_gui
 from tests.test_integration import test_eyes
-
+from particletracker import batchprocess
 
 if __name__ == '__main__':
-    track_gui("testdata/bkg_test")
+    #track_gui("testdata/bkg_test")
     #test_eyes()
+
+    moviefilter = 'E:/RawData/Oliver/orderphobic/Intruders/2025_10_13/630_1/*.mp4'
+    settings = 'E:/RawData/Oliver/orderphobic/Intruders/2025_10_10/param.param'
+    batchprocess(moviefilter, settings)
